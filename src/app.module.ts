3a5c43c9e1212413820f0ee03d0c55b0fac22788
petro1909 <petya-97_97@mail.ru>
2023-07-20 12:05:44 +0300

    feat: register all controllers to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { ArtistService } from './service/app.artistService';
 import { AlbumService } from './service/app.albumService';
 import { TrackService } from './service/app.trackService';
 import { FavoritesService } from './service/app.favoritesService';
+import { ArtistController } from './controller/app.artistController';
+import { AlbumController } from './controller/app.albumController';
+import { TrackController } from './controller/app.trackController';
+import { FavoritesController } from './controller/app.favoritesController';
 
 @Module({
   imports: [],
-  controllers: [UserController],
+  controllers: [UserController, ArtistController, AlbumController, TrackController, FavoritesController],
   providers: [UserService, ArtistService, AlbumService, TrackService, FavoritesService],
 })
 export class AppModule {}
