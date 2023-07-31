70888d93e750c7575b52b680768d7e286808dbed
petro1909 <petya-97_97@mail.ru>
2023-08-01 00:50:11 +0300

    refactor: update import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
 import { config } from 'dotenv';
 import { resolve } from 'path';
 import { serve, setup } from 'swagger-ui-express';
