a707efb2fbed8f9590be2deef75239464f12a6b7
petro1909 <petya-97_97@mail.ru>
2023-08-05 18:14:27 +0300

    refactor: change variable PORT to APP_PORT

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app.module';
-import { config } from 'dotenv';
-import { resolve } from 'path';
 import { serve, setup } from 'swagger-ui-express';
 import { readFile } from 'fs/promises';
 import { load } from 'js-yaml';
 
-config({ path: resolve(process.cwd(), '../.env') });
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.APP_PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,5 +13,6 @@ async function bootstrap() {
   app.use('/doc', serve, setup(document));
 
   await app.listen(PORT);
+  console.log(`Server starts at port ${PORT}`);
 }
 bootstrap();
