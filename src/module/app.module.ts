c8765e65f51b5da9c450a76d1a565a609a9af921
petro1909 <petya-97_97@mail.ru>
2023-08-05 18:15:16 +0300

    feat: add ConfigModule to AppModule

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -5,8 +5,9 @@ import { AlbumModule } from './app.albumModule';
 import { TrackModule } from './app.trackModule';
 import { FavoritesModule } from './app.favoritesModule';
 import { DatabaseModule } from './app.databaseModule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ArtistModule, AlbumModule, TrackModule, FavoritesModule, DatabaseModule],
+  imports: [ConfigModule.forRoot(), UserModule, ArtistModule, AlbumModule, TrackModule, FavoritesModule, DatabaseModule],
 })
 export class AppModule {}
