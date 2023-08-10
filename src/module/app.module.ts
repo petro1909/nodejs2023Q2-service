2950be47646fb3ff1059802f77f37ec7c1c1f5e2
petro1909 <petya-97_97@mail.ru>
2023-08-10 11:13:35 +0300

    feat: add middleware to log request and response

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserModule } from './app.userModule';
 import { ArtistModule } from './app.artistModule';
 import { AlbumModule } from './app.albumModule';
@@ -6,8 +6,13 @@ import { TrackModule } from './app.trackModule';
 import { FavoritesModule } from './app.favoritesModule';
 import { DatabaseModule } from './app.databaseModule';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingMiddleware } from 'src/logging/app.loggingMiddleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, ArtistModule, AlbumModule, TrackModule, FavoritesModule, DatabaseModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
