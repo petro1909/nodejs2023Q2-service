d2d808a1c495011335c1941245679998de1d8c1c
petro1909 <petya-97_97@mail.ru>
2023-08-11 10:29:06 +0300

    feat: add UserService to export

--- src/module/app.userModule.ts
+++ src/module/app.userModule.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from '../controller/app.userController';
-import { UserRepository } from '../repository/userRepositoty';
+import { UserRepository } from '../repository/userRepository';
 import { UserService } from '../service/app.userService';
 
 @Module({
   controllers: [UserController],
   providers: [UserService, UserRepository],
+  exports: [UserService],
 })
 export class UserModule {}
