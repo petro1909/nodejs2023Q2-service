2b0521aef3c44b37310b36ca0178e86f87f0c661
petro1909 <petya-97_97@mail.ru>
2023-08-11 12:47:20 +0300

    refactor: remove ConfigService from providers

--- src/module/app.authModule.ts
+++ src/module/app.authModule.ts
@@ -4,7 +4,6 @@ import { JwtService } from '@nestjs/jwt';
 import { AuthController } from 'src/controller/app.authController';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from 'src/guard/app.authGuard';
-import { ConfigService } from '@nestjs/config';
 import { AuthService } from 'src/service/app.authService';
 
 @Module({
@@ -14,7 +13,6 @@ import { AuthService } from 'src/service/app.authService';
       provide: APP_GUARD,
       useClass: AuthGuard,
     },
-    ConfigService,
     AuthService,
     JwtService,
   ],
