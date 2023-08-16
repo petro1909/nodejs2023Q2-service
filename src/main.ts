2fa68d605a60be0325765f2c0da3bb97f5d6b466
petro1909 <petya-97_97@mail.ru>
2023-08-16 13:34:51 +0300

    feat: set CustomLoggerService as default logger

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './module/app.module';
 import { serve, setup } from 'swagger-ui-express';
 import { readFile } from 'fs/promises';
 import { load } from 'js-yaml';
+import { CustomLoggerService } from './service/app.loggingService';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(CustomLoggerService));
 
   const document = load((await readFile('./doc/api.yaml')).toString());
   app.use('/doc', serve, setup(document));
@@ -15,4 +17,5 @@ async function bootstrap() {
   await app.listen(PORT);
   console.log(`Server starts at port ${PORT}`);
 }
+
 bootstrap();
